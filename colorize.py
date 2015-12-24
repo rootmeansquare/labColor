c5cd019b86efd2affe5df6bcb698b86b5df4fae2
Imaad Umar <imaadumar@gmail.com>
2015-12-24 05:38:42 -0500

    works on multiple photos, tonning down the color

--- colorize.py
+++ colorize.py
@@ -1,37 +1,35 @@
 import cv2 
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
 
-image = cv2.imread('/home/imaad/Downloads/8.jpg')
+#Put this script in folder with two direcrtories inside: toColorize, colorized
+#toColorized contains images that are to be enhanced
+#colorized are the resulting images 
 
-image = cv2.cvtColor(image,cv2.COLOR_RGB2LAB)
+def expandColors(image):
 
-lChl, aChl, bChl = cv2.split(image)
+	lChl, aChl, bChl = cv2.split(image)
 
-eq = cv2.createCLAHE()
-eq.setClipLimit(1)
+	eq = cv2.createCLAHE()
+	eq.setClipLimit(0.7)
 
-aChl =eq.apply(aChl)
-bChl =eq.apply(bChl)
+	aChl =eq.apply(aChl)
+	bChl =eq.apply(bChl)
 
-eq.setClipLimit(0.5)
-lChl = eq.apply(lChl)
+	eq.setClipLimit(0.5)
+	lChl = eq.apply(lChl)
 
-'''
+	result = cv2.merge((lChl,aChl,bChl))
 
-aChl = aChl/(np.linalg.norm(aChl))*100*127
+	result = cv2.cvtColor(result,cv2.COLOR_LAB2BGR)
 
-bChl = bChl/(np.linalg.norm(bChl))*100*127
+	return (result)
 
-aChl = aChl.astype(np.uint8)
-bChl = bChl.astype(np.uint8)
-'''
-result = cv2.merge((lChl,aChl,bChl))
+imList = glob.glob('toColorize/*')
 
-result = cv2.cvtColor(result,cv2.COLOR_LAB2RGB)
-
-cv2.imwrite('/home/imaad/Downloads/result.jpg',result)
-
-plt.figure('')
-plt.imshow(image)
-plt.imshow(result)
+for idx,i in enumerate(imList):
+	image = cv2.imread(i)
+	image = cv2.cvtColor(image,cv2.COLOR_BGR2LAB)
+	colorizedImage = expandColors(image)
+	cv2.imwrite('colorized/' +str(idx) + '.jpg',colorizedImage)
